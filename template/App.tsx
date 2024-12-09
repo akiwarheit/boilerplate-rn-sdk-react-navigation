29a01c8fcb99e0545fe38aac0f4a88e9ff5bc1eb
Kevin Deloria <kevin@rushdigital.co.nz>
2024-12-10 10:52:47 +1300

    Adding react-nav

--- template/App.tsx
+++ template/App.tsx
@@ -2,12 +2,14 @@
  * Boilerplate React Native App For Atomic SDK
  */
 
-import React from 'react';
-import {SafeAreaView} from 'react-native';
 import {
   Session as AtomicSession,
   StreamContainer,
 } from '@atomic.io/react-native-atomic-sdk';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
 
 const ATOMIC_API_HOST = '';
 const ATOMIC_API_KEY = '';
@@ -26,16 +28,47 @@ AtomicSession.initialise(ATOMIC_ENVIRONMENT_ID, ATOMIC_API_KEY);
 AtomicSession.setApiBaseUrl(ATOMIC_API_HOST);
 AtomicSession.setSessionDelegate(onAuthTokenRequested);
 
-const App = () => {
+const ContainerScreen = () => {
+  const {goBack} = useNavigation();
   return (
-    <SafeAreaView>
+    <View>
       <StreamContainer
         style={{width: '100%', height: '100%'}}
         containerId={ATOMIC_STREAM_CONTAINER_ID}
         configuration={{}}
       />
-    </SafeAreaView>
+      <Button onPress={goBack} title="Back" />
+    </View>
   );
 };
+
+const NoContainerScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Button
+        onPress={() => {
+          // @ts-ignore
+          navigation.navigate('SecondPage');
+        }}
+        title="Go To Container"
+      />
+    </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={NoContainerScreen} />
+        <Stack.Screen name="SecondPage" component={ContainerScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
